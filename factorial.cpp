2e8f2c24771a0d27107a85a17876365859c8da47
Lakshya Kapoor <Lakshya.Kapoor@iiitb.ac.in>
2024-02-16 18:30:22 +0530

    problem codes

---
+++ factorial.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n = 5;
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
+        fact*=i;
+    cout << "Factorial is" << fact << endl;
+    return 0;
+}
