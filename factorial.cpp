f5ecd698eed3772336959f0983c6b7c7fa950b35
Lakshya Kapoor <Lakshya.Kapoor@iiitb.ac.in>
2024-02-25 16:09:03 +0530

    Final processor ready

--- factorial.cpp
+++ factorial.cpp
@@ -1,11 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     int n = 5;
     int fact = 1;
-    for (int i = 1; i <= n; i++)
-        fact*=i;
+    for (int i = 1; i <= n; i++) fact *= i;
     cout << "Factorial is" << fact << endl;
     return 0;
 }
